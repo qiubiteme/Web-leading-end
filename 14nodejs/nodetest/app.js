316556e4afdb773b3ca9450d091af76dad3adbbc
qiubite <meqiuayng@gmail.com>
2018-07-02 17:21:56 +0800

    koa-router 配置使用

--- 14nodejs/nodetest/app.js
+++ 14nodejs/nodetest/app.js
@@ -1,14 +1,19 @@
 // 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
 const Koa = require('koa');
 
+// 注意require('koa-router')返回的是函数:
+const router = require('koa-router')();
+
 // 创建一个Koa对象表示web app本身:
 const app = new Koa();
 
+//打印请求的url地址
 app.use(async (ctx, next) => {
     console.log(`${ctx.request.method} ${ctx.request.url}`); // 打印URL
     await next(); // 调用下一个middleware
 });
 
+//打印请求消耗时间
 app.use(async (ctx, next) => {
     const start = new Date().getTime(); // 当前时间
     await next(); // 调用下一个middleware
@@ -16,15 +21,22 @@ app.use(async (ctx, next) => {
     console.log(`Time: ${ms}ms`); // 打印耗费时间
 });
 
-// 对于任何请求，app将调用该异步函数处理请求：
-app.use(async (ctx, next) => {
-    await next();
-    // 设置response的Content-Type:
-    ctx.response.type = 'text/html';
-    // 设置response的内容:
-    ctx.response.body = '<h1>Hello,word , koa2!</h1>';
+
+
+// 带参数的请求
+router.get('/hello/:name', async (ctx, next) => {
+    var name = ctx.params.name;
+    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
 });
+
+//默认请求处理
+router.get('/', async (ctx, next) => {
+    ctx.response.body = '<h1>Index</h1>';
+});
+
+// add koa-router to middleware:添加koa-router路由中间件
+app.use(router.routes());
 
 // 在端口3000监听:
 app.listen(3000);
-console.log('app started at port 3000...');
+console.log('app started at port 3000... 访问 http://127.0.0.1:3000/ 查看内容');
