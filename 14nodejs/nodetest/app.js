b91896deeaed19f58cc4a03976ab9f786a627f08
qiubite <meqiuayng@gmail.com>
2018-07-02 17:36:47 +0800

    koa-bodyparser 数据绑定

--- 14nodejs/nodetest/app.js
+++ 14nodejs/nodetest/app.js
@@ -1,8 +1,11 @@
 // 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
 const Koa = require('koa');
 
-// 注意require('koa-router')返回的是函数:
+// 用来处理url的,注意require('koa-router')返回的是函数:
 const router = require('koa-router')();
+
+//用于绑定ctx.request.body数据
+const bodyParser = require('koa-bodyparser');
 
 // 创建一个Koa对象表示web app本身:
 const app = new Koa();
@@ -21,18 +24,32 @@ app.use(async (ctx, next) => {
     console.log(`Time: ${ms}ms`); // 打印耗费时间
 });
 
-
-
-// 带参数的请求
-router.get('/hello/:name', async (ctx, next) => {
-    var name = ctx.params.name;
-    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
-});
-
-//默认请求处理
+//默认的get请求
 router.get('/', async (ctx, next) => {
-    ctx.response.body = '<h1>Index</h1>';
+    ctx.response.body = `<h1>Index</h1>
+        <form action="/signin" method="post">
+            <p>Name: <input name="name" value="koa"></p>
+            <p>Password: <input name="password" type="password"></p>
+            <p><input type="submit" value="提交"></p>
+        </form>`;
 });
+
+//接收post请求
+router.post('/signin', async (ctx, next) => {
+    var name = ctx.request.body.name || '';
+    var password = ctx.request.body.password || '';
+    console.log(`signin with name: ${name}, password: ${password}`);
+    //测试提交账号密码
+    if (name === 'koa' && password === '12345') {
+        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+    } else {
+        ctx.response.body = `<h1>Login failed!</h1>
+        <p><a href="/">Try again</a></p>`;
+    }
+});
+
+// add 用于绑定ctx.request.body数据,处理
+app.use(bodyParser());
 
 // add koa-router to middleware:添加koa-router路由中间件
 app.use(router.routes());
